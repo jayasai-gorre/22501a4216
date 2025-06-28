ea95867c482699a2c3f0cd360045fbf7da2ea06c
TheAtoms <123870905+jayasai-gorre@users.noreply.github.com>
2025-06-28 12:49:45 +0530

    corrected error in logging

--- Logging Middleware/logging.js
+++ Logging Middleware/logging.js
@@ -1,18 +1,3 @@
-// import fs from "fs";
-
-// const logToFile = ({name, location, mainUrl, shortenedUrl, expiresAt}) => {
-//     const timeStamp = new Date().toISOString();
-//     const logEntry = `[${timeStamp}] name=${name} location=${location} mailUrl=${mainUrl} shortenedUrl=${shortenedUrl} expiresAt=${expiresAt}\n`;
-
-//     fs.appendFile("log.txt", logEntry, throwErrorHandler);
-// }
-
-// const throwErrorHandler = (err) => {
-//     if (err) throw err;
-// }
-
-// export default logToFile;
-
 import fs from "fs";
 
 const throwErrorHandler = (err) => {
